270678b816d53bf71e14e933186719bf2dd3f6f6
lilingfengdev <lilingfengdev@outlook.com>
2024-05-28 23:21:19 +0800

    win7还是要的

--- install-python.bat
+++ install-python.bat
@@ -1,17 +1,8 @@
 @echo off
 chcp 65001
-echo SBFactory.newInstance()
 echo 开始下载Python
-curl -L -o python-installer.exe https://mirrors.huaweicloud.com/python/3.8.10/python-3.8.10.exe
+curl -L -o python-installer.exe https://raw.githubusercontent.com/adang1345/PythonWin7/master/3.11.9/python-3.11.9-amd64-full.exe
 echo 下载完成
 echo 开始安装
 python-installer.exe /quiet CompileAll=1 SimpleInstallDescription="笨蛋脚本Python自动安装程序" Include_test=0 PrependPath=1
 echo 安装完成
-echo 清理安装文件
-del /Q /S "python-installer.exe"
-echo 清理完成
-echo 下载前置库
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-pip install pyyaml psutil install-jdk
-echo 前置库下载完成
-pause
