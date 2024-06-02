f5846dfdefab0a1f2003949f063399db4141dab7
lilingfengdev <lilingfengdev@outlook.com>
2024-06-02 15:25:20 +0800

    fix action

--- generate-bundle.py
+++ generate-bundle.py
@@ -8,8 +8,8 @@ import PyInstaller.__main__
 urllib.request.urlretrieve("https://github.com/upx/upx/releases/download/v4.2.4/upx-4.2.4-win64.zip",
                            "upx.zip")
 zip = zipfile.ZipFile("upx.zip")
-zip.extract("upx/upx.exe", path=os.getcwd())
-shutil.move("upx/upx.exe", os.path.join(os.getcwd(), "upx.exe"))
+zip.extract("upx-4.2.4-win64/upx.exe", path=os.getcwd())
+shutil.move("upx-4.2.4-win64/upx.exe", os.path.join(os.getcwd(), "upx.exe"))
 os.mkdir("dist")
 for file in os.listdir(os.getcwd()):
     if file != "utils.py" and file != "generate-bundle.py" and file.endswith(".py") and not os.path.isdir(file):
